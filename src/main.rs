4fa36c9c48d49e81bb5c87ddf73e94d4f41fecb4
Rafael Milewski <rafael.milewski@gmail.com>
2022-12-04 12:18:04 +0800

    temp

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,44 @@ use std::borrow::Cow;
 use std::time::{Duration, Instant};
 
 use glutin_window::GlutinWindow;
-use graphics::image as create_image;
+use graphics::{image as create_image};
 use image::{DynamicImage, GenericImageView, Pixel};
-use image::imageops::blur;
+use image::imageops::{blur, FilterType};
 use nokhwa::{Camera, CameraFormat, FrameFormat};
 use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings};
-use piston::event_loop::{Events, EventSettings};
 use piston::RenderEvent;
+use piston::event_loop::{Events, EventSettings};
 use piston::window::WindowSettings;
 use rustface::{Detector, ImageData};
 
-static WIDTH: u32 = 640;
-static HEIGHT: u32 = 480;
+#[derive(PartialEq)]
+struct Resolution {
+    width: u32,
+    height: u32,
+}
+
+struct Settings {
+    framerate: u32,
+    capture: Resolution,
+    detection: Resolution,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Settings {
+            capture: Resolution { width: 1280, height: 720 },
+            detection: Resolution { width: 1280, height: 720 },
+            // detection: Resolution { width: 640 - 200, height: 480 - 200 },
+            framerate: 30,
+        }
+    }
+}
 
 fn main() {
+    let options: Settings = Settings {
+        ..Settings::default()
+    };
+
     let mut detector = match rustface::create_detector("./model/seeta_fd_frontal_v1.0.bin") {
         Ok(detector) => detector,
         Err(error) => {
@@ -31,14 +55,15 @@ fn main() {
     detector.set_pyramid_scale_factor(0.7);
     detector.set_slide_window_step(5, 5);
 
-    let mut camera = Camera::new(1, Some(CameraFormat::new_from(WIDTH, HEIGHT, FrameFormat::MJPEG, 30)))
+    let format = CameraFormat::new_from(options.capture.width, options.capture.height, FrameFormat::MJPEG, options.framerate);
+    let mut camera = Camera::new(1, Some(format))
         .unwrap();
 
     camera.open_stream().unwrap();
 
-    let opengl = OpenGL::V3_2;
+    let opengl = OpenGL::V4_5;
 
-    let mut window: GlutinWindow = WindowSettings::new("capture", [WIDTH, HEIGHT])
+    let mut window: GlutinWindow = WindowSettings::new("capture", [options.capture.width, options.capture.height])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -50,7 +75,7 @@ fn main() {
     while let Some(event) = events.next(&mut window) {
         if let Some(args) = event.render_args() {
             let buffer = camera.frame_raw().unwrap();
-            let texture = get_image_from_frame(&mut *detector, buffer);
+            let texture = get_image_from_frame(&mut *detector, buffer, &options);
 
             gl.draw(args.viewport(), |c, g| {
                 create_image(&texture, c.transform, g);
@@ -63,14 +88,23 @@ fn get_millis(duration: Duration) -> u64 {
     duration.as_secs() * 1000u64 + u64::from(duration.subsec_nanos() / 1_000_000)
 }
 
-fn get_image_from_frame(detector: &mut dyn Detector, buffer: Cow<[u8]>) -> Texture {
+fn detect_face(detector: &mut dyn Detector, options: &Settings) {
+
+}
+
+fn get_image_from_frame(detector: &mut dyn Detector, buffer: Cow<[u8]>, options: &Settings) -> Texture {
     let now = Instant::now();
+
     let image: DynamicImage = image::load_from_memory(&buffer).unwrap();
+    let lower_image: DynamicImage = image
+        .resize_exact(options.detection.width, options.detection.height, FilterType::Nearest);
 
+    let transparent_image = DynamicImage::new_rgba8(options.detection.width, options.detection.height);
+    let mut transparent_rgba = transparent_image.to_rgba8();
     let mut rgba = image.to_rgba8();
-    let luma = image.to_luma8();
 
-    let mut image_data = ImageData::new(&luma, luma.width(), luma.height());
+    let lower_luma = lower_image.to_luma8();
+    let mut image_data = ImageData::new(&lower_luma, options.detection.width, options.detection.height);
     let faces = detector.detect(&mut image_data);
 
     for face in faces {
@@ -78,13 +112,26 @@ fn get_image_from_frame(detector: &mut dyn Detector, buffer: Cow<[u8]>) -> Textu
         let box_x = bbox.x() as u32;
         let box_y = bbox.y() as u32;
 
-        let cropped = rgba.view(box_x, box_y, bbox.width(), bbox.height()).to_image();
+        let cropped = lower_image.view(box_x, box_y, bbox.width(), bbox.height()).to_image();
         let blurred = blur(&cropped, 5.0);
 
         for (x, y, pixel) in blurred.enumerate_pixels() {
             let offset_x = x + box_x;
             let offset_y = y + box_y;
-            rgba.put_pixel(offset_x, offset_y, pixel.to_rgba())
+            transparent_rgba.put_pixel(offset_x, offset_y, pixel.to_rgba())
+        }
+    }
+
+    let resized_image = DynamicImage::from(transparent_rgba)
+        .resize_exact(options.capture.width, options.capture.height, FilterType::Nearest)
+        .to_rgba8();
+
+    for (x, y, pixel) in resized_image.enumerate_pixels() {
+        match pixel.0 {
+            [0, 0, 0, 0] => (),
+            _ => {
+                rgba.put_pixel(x, y, pixel.to_rgba())
+            }
         }
     }
 
